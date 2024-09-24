645927abf8886161bd75e49024ab820de538dc1e
pollyndos <56310766+pollyndos@users.noreply.github.com>
2024-09-24 20:42:08 +0400

    Update Program.cs

--- asp-net-core-server/Program.cs
+++ asp-net-core-server/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 namespace AspNetCoreDashboardBackend {
     public class Program {
         public static void Main(string[] args) {
+            DevExpress.Utils.DeserializationSettings.RegisterTrustedClass(typeof(TasksData));
             CreateHostBuilder(args).Build().Run();
         }
 
